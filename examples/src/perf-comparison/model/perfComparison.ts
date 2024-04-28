04eae01d3716ccaab7652d3a3b19d2115c45fe48
Matt Rakow <matt.rakow@gmail.com>
2024-04-27 21:55:34 -0700

    Add eventlet test

--- examples/src/perf-comparison/model/perfComparison.ts
+++ examples/src/perf-comparison/model/perfComparison.ts
@@ -1,17 +1,20 @@
-// import { Eventlet } from "eventlet";
-// import { EventEmitter } from "events";
-
+import { Eventlet } from "eventlet";
 import { EventEmitter } from "events";
+
+const REGISTER_UNREGISTER_ITERATION_COUNT = 10_000_000;
+const listeners: (() => void)[] = [];
+listeners.length = REGISTER_UNREGISTER_ITERATION_COUNT;
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+listeners.fill(() => {});
 
 export class PerfComparison {
     public readonly runRegisterUnregisterTestEventEmitter = () => {
-        const ITERATION_COUNT = 10_000_000;
         const eventEmitter = new EventEmitter();
         eventEmitter.setMaxListeners(0);
         const eventName = "a";
 
         const phase1StartTime = Date.now();
-        for (let i = 0; i < ITERATION_COUNT; i++) {
+        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
             // eslint-disable-next-line @typescript-eslint/no-empty-function
             const eventListener = () => {};
             eventEmitter.on(eventName, eventListener);
@@ -21,13 +24,8 @@ export class PerfComparison {
         const phase1Duration = phase1EndTime - phase1StartTime;
         console.log(`runRegisterUnregisterTestEventEmitter phase 1: ${ phase1Duration }`);
 
-        const listeners: (() => void)[] = [];
-        listeners.length = ITERATION_COUNT;
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        listeners.fill(() => {});
-
         const phase2StartTime = Date.now();
-        for (let i = 0; i < ITERATION_COUNT; i++) {
+        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             eventEmitter.on(eventName, listeners[i]!);
         }
@@ -36,7 +34,7 @@ export class PerfComparison {
         console.log(`runRegisterUnregisterTestEventEmitter phase 2: ${ phase2Duration }`);
 
         const phase3StartTime = Date.now();
-        for (let i = 0; i < ITERATION_COUNT; i++) {
+        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             eventEmitter.off(eventName, listeners[i]!);
         }
@@ -48,7 +46,38 @@ export class PerfComparison {
     };
 
     public readonly runRegisterUnregisterTestEventlet = () => {
-        return 3;
+        const eventEmitter = new Eventlet();
+
+        const phase1StartTime = Date.now();
+        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            const eventListener = () => {};
+            eventEmitter.add(eventListener);
+            eventEmitter.remove(eventListener);
+        }
+        const phase1EndTime = Date.now();
+        const phase1Duration = phase1EndTime - phase1StartTime;
+        console.log(`runRegisterUnregisterTestEventlet phase 1: ${ phase1Duration }`);
+
+        const phase2StartTime = Date.now();
+        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            eventEmitter.add(listeners[i]!);
+        }
+        const phase2EndTime = Date.now();
+        const phase2Duration = phase2EndTime - phase2StartTime;
+        console.log(`runRegisterUnregisterTestEventEmitter phase 2: ${ phase2Duration }`);
+
+        const phase3StartTime = Date.now();
+        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            eventEmitter.remove(listeners[i]!);
+        }
+        const phase3EndTime = Date.now();
+        const phase3Duration = phase3EndTime - phase3StartTime;
+        console.log(`runRegisterUnregisterTestEventEmitter phase 3: ${ phase3Duration }`);
+
+        return phase1Duration + phase2Duration + phase3Duration;
     };
 
     public readonly runEmitTestEventEmitter = () => {
