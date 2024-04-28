18ea3ce0b16ede93a93fc8fe526b00835819a057
Matt Rakow <matt.rakow@gmail.com>
2024-04-27 22:56:41 -0700

    Add emit tests

--- examples/src/perf-comparison/model/perfComparison.ts
+++ examples/src/perf-comparison/model/perfComparison.ts
@@ -1,11 +1,10 @@
 import { Eventlet } from "eventlet";
 import { EventEmitter } from "events";
 
-const REGISTER_UNREGISTER_ITERATION_COUNT = 10_000_000;
-const listeners: (() => void)[] = [];
-listeners.length = REGISTER_UNREGISTER_ITERATION_COUNT;
+const ITERATION_COUNT = 100_000;
+const MANY_EMIT_ITERATION_COUNT = 10;
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-listeners.fill(() => {});
+const listeners: (() => void)[] = Array.from({ length: ITERATION_COUNT }, () => (() => {}));
 
 export class PerfComparison {
     public readonly runRegisterUnregisterTestEventEmitter = () => {
@@ -14,7 +13,7 @@ export class PerfComparison {
         const eventName = "a";
 
         const phase1StartTime = Date.now();
-        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
+        for (let i = 0; i < ITERATION_COUNT; i++) {
             // eslint-disable-next-line @typescript-eslint/no-empty-function
             const eventListener = () => {};
             eventEmitter.on(eventName, eventListener);
@@ -25,7 +24,7 @@ export class PerfComparison {
         console.log(`runRegisterUnregisterTestEventEmitter phase 1: ${ phase1Duration }`);
 
         const phase2StartTime = Date.now();
-        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
+        for (let i = 0; i < ITERATION_COUNT; i++) {
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             eventEmitter.on(eventName, listeners[i]!);
         }
@@ -34,7 +33,8 @@ export class PerfComparison {
         console.log(`runRegisterUnregisterTestEventEmitter phase 2: ${ phase2Duration }`);
 
         const phase3StartTime = Date.now();
-        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
+        // for (let i = ITERATION_COUNT - 1; i >= 0; i--) {
+        for (let i = 0; i < ITERATION_COUNT; i++) {
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             eventEmitter.off(eventName, listeners[i]!);
         }
@@ -49,7 +49,7 @@ export class PerfComparison {
         const eventEmitter = new Eventlet();
 
         const phase1StartTime = Date.now();
-        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
+        for (let i = 0; i < ITERATION_COUNT; i++) {
             // eslint-disable-next-line @typescript-eslint/no-empty-function
             const eventListener = () => {};
             eventEmitter.add(eventListener);
@@ -60,31 +60,88 @@ export class PerfComparison {
         console.log(`runRegisterUnregisterTestEventlet phase 1: ${ phase1Duration }`);
 
         const phase2StartTime = Date.now();
-        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
+        for (let i = 0; i < ITERATION_COUNT; i++) {
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             eventEmitter.add(listeners[i]!);
         }
         const phase2EndTime = Date.now();
         const phase2Duration = phase2EndTime - phase2StartTime;
-        console.log(`runRegisterUnregisterTestEventEmitter phase 2: ${ phase2Duration }`);
+        console.log(`runRegisterUnregisterTestEventlet phase 2: ${ phase2Duration }`);
 
         const phase3StartTime = Date.now();
-        for (let i = 0; i < REGISTER_UNREGISTER_ITERATION_COUNT; i++) {
+        // for (let i = ITERATION_COUNT - 1; i >= 0; i--) {
+        for (let i = 0; i < ITERATION_COUNT; i++) {
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             eventEmitter.remove(listeners[i]!);
         }
         const phase3EndTime = Date.now();
         const phase3Duration = phase3EndTime - phase3StartTime;
-        console.log(`runRegisterUnregisterTestEventEmitter phase 3: ${ phase3Duration }`);
+        console.log(`runRegisterUnregisterTestEventlet phase 3: ${ phase3Duration }`);
 
         return phase1Duration + phase2Duration + phase3Duration;
     };
 
     public readonly runEmitTestEventEmitter = () => {
-        return 3;
+        const eventEmitter = new EventEmitter();
+        eventEmitter.setMaxListeners(0);
+        const eventName = "a";
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const eventListener = () => {};
+        eventEmitter.on(eventName, eventListener);
+
+        const phase1StartTime = Date.now();
+        for (let i = 0; i < ITERATION_COUNT; i++) {
+            eventEmitter.emit(eventName);
+        }
+        const phase1EndTime = Date.now();
+        const phase1Duration = phase1EndTime - phase1StartTime;
+        console.log(`runEmitTestEventEmitter phase 1: ${ phase1Duration }`);
+
+        eventEmitter.off(eventName, eventListener);
+        for (let i = 0; i < ITERATION_COUNT; i++) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            eventEmitter.on(eventName, listeners[i]!);
+        }
+
+        const phase2StartTime = Date.now();
+        for (let i = 0; i < MANY_EMIT_ITERATION_COUNT; i++) {
+            eventEmitter.emit(eventName);
+        }
+        const phase2EndTime = Date.now();
+        const phase2Duration = phase2EndTime - phase2StartTime;
+        console.log(`runEmitTestEventEmitter phase 2: ${ phase2Duration }`);
+
+        return phase1Duration + phase2Duration;
     };
 
     public readonly runEmitTestEventlet = () => {
-        return 3;
+        const eventlet = new Eventlet();
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const eventListener = () => {};
+        eventlet.add(eventListener);
+
+        const phase1StartTime = Date.now();
+        for (let i = 0; i < ITERATION_COUNT; i++) {
+            eventlet.emit();
+        }
+        const phase1EndTime = Date.now();
+        const phase1Duration = phase1EndTime - phase1StartTime;
+        console.log(`runEmitTestEventEmitter phase 1: ${ phase1Duration }`);
+
+        eventlet.remove(eventListener);
+        for (let i = 0; i < ITERATION_COUNT; i++) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            eventlet.add(listeners[i]!);
+        }
+
+        const phase2StartTime = Date.now();
+        for (let i = 0; i < MANY_EMIT_ITERATION_COUNT; i++) {
+            eventlet.emit();
+        }
+        const phase2EndTime = Date.now();
+        const phase2Duration = phase2EndTime - phase2StartTime;
+        console.log(`runEmitTestEventEmitter phase 2: ${ phase2Duration }`);
+
+        return phase1Duration + phase2Duration;
     };
 }
